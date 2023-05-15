cc58d7ba0ca875ce8eed381cde037356f00da182
JacobLi11 <Jacob.Li@splashtop.com>
2023-05-15 17:16:04 +0800

    chore: change connector file
    Signed-off-by: JacobLi11 <Jacob.Li@splashtop.com>

--- connector.rb
+++ connector.rb
@@ -18,11 +18,11 @@ class Connector < Formula
       def install
         bin.install Dir["*"].first => "connector"
         # Install bash completion
-        output = Utils.safe_popen_read("#{bin}/connector", "-h")
-        (bash_completion/"connector").write output
+        # output = Utils.safe_popen_read("#{bin}/connector", "-h")
+        # (bash_completion/"connector").write output
         # Install zsh completion
-        output = Utils.safe_popen_read("#{bin}/connector", "-h")
-        (zsh_completion/"_connector").write output
+        # output = Utils.safe_popen_read("#{bin}/connector", "-h")
+        # (zsh_completion/"_connector").write output
       end
     end
     if Hardware::CPU.intel?
