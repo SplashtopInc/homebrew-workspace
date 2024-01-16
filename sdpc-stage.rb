fa9c1cb0643541301ff45eade6adea45a2faa7f1
sep-ops <sep-ops@users.noreply.github.com>
2024-01-16 06:32:22 +0000

    add sdpc version

--- sdpc-stage.rb
+++ sdpc-stage.rb
@@ -13,7 +13,7 @@ class SdpcStage < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://s3.us-west-2.amazonaws.com/splashlock-assets.splashshield.ai/sdpc/stage/darwin/arm64/sdpc-darwin-arm64-0.1.101-stage-1.0.21.9.zip"
-      sha256 "56ceadcde8ee7554ee27630fadd6f81f3ec9c06534223457706195a808a06a59"
+      sha256 "51e5b22b58062bd0619ea635831d203fbe8733ea36e2635205179140dd913840"
 
       def install
         bin.install "sdpc-darwin-arm64" => "sdpc"
@@ -27,7 +27,7 @@ class SdpcStage < Formula
     end
     if Hardware::CPU.intel?
       url "https://s3.us-west-2.amazonaws.com/splashlock-assets.splashshield.ai/sdpc/stage/darwin/amd64/sdpc-darwin-amd64-0.1.101-stage-1.0.21.9.zip"
-      sha256 "cf1c4fd2ff72f66c031243d33cec2fde34597ce9966077b278332138b2a9c806"
+      sha256 "15219ad93b71659394a0f39484b359ad64127143813be86b220f8db01de3d540"
 
       def install
         bin.install "sdpc-darwin-amd64" => "sdpc"
